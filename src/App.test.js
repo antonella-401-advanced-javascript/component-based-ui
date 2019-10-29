6142ebcfac1e1726f9a3c35b6f4c479e9547d0a3
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-29 16:41:20 -0700

    Travis WILL pass now

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { shallow } from 'enzyme';
 import App from './App';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('App component', () => {
   it('renders App', () => {
