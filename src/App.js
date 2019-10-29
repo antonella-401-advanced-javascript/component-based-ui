a881418f172d1625d892eb300d6a05708a93104d
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-29 15:35:02 -0700

    getting lab started

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const App = () => (
+  <React.Fragment>
+    <Header />
+    <Footer />
+  </React.Fragment>
+);
+
+export default App;
