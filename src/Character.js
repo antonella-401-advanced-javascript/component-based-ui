ea6a405263a46f0afaee95884927d2e37d5a78a0
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-10-29 19:25:04 -0700

    done styling

--- src/Character.js
+++ src/Character.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Character.css';
 
 const Character = ({ img, name, species }) => {
   return (
-    <dl>
-      <dt>Image</dt>
-      <dd><img src={img} /></dd>
+    <dl className={styles.Character}>
+      <div className={styles.SecondClass}>
+        <dt>Image</dt>
+        <dd><img src={img} /></dd>
+      </div>
 
-      <dt>Name</dt>
-      <dd>{name}</dd>
+      <div>
+        <dt>Name</dt>
+        <dd>{name}</dd>
+      </div>
 
-      <dt>Species</dt>
-      <dd>{species}</dd>
+      <div>
+        <dt>Species</dt>
+        <dd>{species}</dd>
+      </div>
     </dl>
   );
 };
